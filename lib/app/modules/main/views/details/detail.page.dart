4cfb5130b0aa700f66aa9a34c2e57aaa471ef4cb
avelay <avelay@jexbit.mx>
2022-06-03 16:08:59 -0500

    det

--- lib/app/modules/main/views/details/detail.page.dart
+++ lib/app/modules/main/views/details/detail.page.dart
@@ -4,43 +4,36 @@ import 'package:jexpoints/app/modules/main/views/details/detail.controller.dart'
 
 class DetailPage extends GetView<DetailController> {
   @override
-  Widget build(BuildContext context) {  
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Detalles del Premio'),
+        title: const Text('Detalles del Producto'),
         backgroundColor: const Color(0xFF222222),
       ),
-      body: Column(
-        children: [          
-          _image(controller),
-          const SizedBox(height: 10),
-          _center(context, controller),          
-          const SizedBox(height: 40),
-          _bottomButton(context, controller)
-        ]
-      ),
+      body: Column(children: [
+        _image(controller),
+        const SizedBox(height: 10),
+        _center(context, controller),
+        const SizedBox(height: 40),
+        _bottomButton(context, controller)
+      ]),
     );
   }
 
-  static Widget _image(DetailController controller){
+  static Widget _image(DetailController controller) {
     return SizedBox(
-      width: double.infinity,
-      child: FadeInImage(
-        fit: BoxFit.contain,
-        placeholder: const AssetImage('assets/images/loading.gif'),
-        image: NetworkImage(controller.imageUrl),
-      )
-    );
+        width: double.infinity,
+        child: FadeInImage(
+          fit: BoxFit.contain,
+          placeholder: const AssetImage('assets/images/loading.gif'),
+          image: NetworkImage(controller.imageUrl),
+        ));
   }
 
-  static Widget _center(BuildContext context, DetailController controller){
+  static Widget _center(BuildContext context, DetailController controller) {
     return Column(
       children: [
-        Text(controller.name.toString(),
-            style: const TextStyle(
-              fontSize: 25
-            )
-        ),
+        Text(controller.name.toString(), style: const TextStyle(fontSize: 25)),
         const Padding(
           padding: EdgeInsets.all(10.0),
           child: Text(
@@ -51,36 +44,33 @@ class DetailPage extends GetView<DetailController> {
     );
   }
 
-  static Widget _bottomButton(BuildContext context, DetailController controller){
+  static Widget _bottomButton(
+      BuildContext context, DetailController controller) {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 10),
-      padding: const EdgeInsets.symmetric(horizontal: 15),      
+      padding: const EdgeInsets.symmetric(horizontal: 15),
       child: GestureDetector(
         onTap: () {
-          Get.toNamed('generate-qr', arguments: { "type" : "change" });
+          Get.toNamed('generate-qr', arguments: {"type": "change"});
         },
         child: Container(
           height: 60,
           decoration: BoxDecoration(
-            color: const Color(0xFF222222),
-            borderRadius: BorderRadius.circular(20)
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [              
-              const Icon(
-                Icons.qr_code_2,
-                color: Colors.white,
-              ),
-              const SizedBox(width: 10),
-              Text(
-                'Canjear ${controller.points} pts',
-                style: const TextStyle(color: Colors.white, fontSize: 24),
-              )
-            ]
-          ),
+              color: const Color(0xFF222222),
+              borderRadius: BorderRadius.circular(20)),
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const Icon(
+              Icons.add_shopping_cart,
+              color: Colors.white,
+            ),
+            const SizedBox(width: 10),
+            Text(
+              'Agregar al carrito \$ ${controller.points} ',
+              style: const TextStyle(color: Colors.white, fontSize: 24),
+            )
+          ]),
         ),
       ),
-    );        
-  } 
+    );
+  }
 }
