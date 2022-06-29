3f99b7cc29a7f43a07fcec86798cb8fd9f0980fc
Miguel Zurita <mzurita@sethemba.mx>
2022-06-29 16:15:02 -0500

    .

--- lib/app/modules/main/views/catalogos/catalogos.page.dart
+++ lib/app/modules/main/views/catalogos/catalogos.page.dart
@@ -2,12 +2,11 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'catagolos.controller.dart';
-
 class CatalogosPage extends GetView<CatalogosController> {
   const CatalogosPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+    Widget build(BuildContext context) {
     return Container(
       color: const Color(0xff222222),
       child: SafeArea(
@@ -28,7 +27,7 @@ class CatalogosPage extends GetView<CatalogosController> {
     );
   }
 
-  Widget _tabBar(context, controller) {
+    Widget _tabBar(context, controller) {
     return Column(
       children: [
         Padding(
@@ -45,10 +44,10 @@ class CatalogosPage extends GetView<CatalogosController> {
               labelColor: Colors.white,
               unselectedLabelColor: Colors.black,
               tabs: const [
-                Tab(
+                 Tab(
                   text: 'Esperanza',
                 ),
-                Tab(
+                 Tab(
                   text: 'Santo Gallo',
                 ),
               ],
@@ -58,10 +57,10 @@ class CatalogosPage extends GetView<CatalogosController> {
         Expanded(
             child: TabBarView(
           children: [
-            Center(
-              child: _ListCatalogue(context, controller),
-            ),
-            Center(child: _ListCatalogue1(context, controller)),
+               Center(
+                child: _ListCatalogue(context, controller),
+              ),
+             Center(child: _ListCatalogue1(context, controller)),
           ],
         ))
       ],
@@ -76,74 +75,67 @@ class CatalogosPage extends GetView<CatalogosController> {
         child: SingleChildScrollView(
           dragStartBehavior: DragStartBehavior.start,
           scrollDirection: Axis.vertical,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 5.0),
-            child: Obx((() {
-              return GridView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: controller.catalogueList$.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 12.0,
-                    crossAxisSpacing: 10.0,
-                    childAspectRatio: 1,
-                  ),
-                  itemBuilder: (context, index) => _ListItemCatalogue(
-                      context, controller.catalogueList$[index]));
-            })),
-          ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5.0),
+              child: Obx((() {
+                return GridView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: controller.catalogueList$.length,
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 12.0,
+                      crossAxisSpacing: 10.0,
+                      childAspectRatio: 1,
+                    ),
+                    itemBuilder: (context, index) => _ListItemCatalogue(
+                        context, controller.catalogueList$[index]));
+              })),
+            ),
         ),
       ),
     ]);
   }
 
   static Widget _ListItemCatalogue(BuildContext context, dynamic product) {
-    return Container(
-      width: 130,
-      height: 190,
-      margin: const EdgeInsets.symmetric(horizontal: 10),
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/catalogs-list');
+      },
       child: Column(
-        children: [
-          GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(context, '/catalogs-list');
-            },
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: FadeInImage(
-                        fit: BoxFit.cover,
-                        placeholder: NetworkImage(product.image),
-                        image: NetworkImage(product.image),
-                      ),
-                    ),
-                  ),
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: FadeInImage(
+                  fit: BoxFit.cover,
+                  placeholder: NetworkImage(product.image),
+                  image: NetworkImage(product.image),
                 ),
-                const SizedBox(height: 3),
-                Text(product.name,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 1,
-                    softWrap: false,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(color: Colors.white)),
-              ],
+              ),
             ),
           ),
+          const SizedBox(height: 3),
+          Text(product.name,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+              softWrap: false,
+              textAlign: TextAlign.center,
+              style: const TextStyle(color: Colors.white)),
         ],
       ),
     );
   }
 
-  static Widget _ListCatalogue1(
+
+
+static Widget _ListCatalogue1(
       BuildContext context, CatalogosController controller) {
     return Column(children: [
       SizedBox(
@@ -151,23 +143,23 @@ class CatalogosPage extends GetView<CatalogosController> {
         child: SingleChildScrollView(
           dragStartBehavior: DragStartBehavior.start,
           scrollDirection: Axis.vertical,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 5.0),
-            child: Obx((() {
-              return GridView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: controller.catalogueList1$.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 12.0,
-                    crossAxisSpacing: 10.0,
-                    childAspectRatio: 1,
-                  ),
-                  itemBuilder: (context, index) => _ListItemCatalogue1(
-                      context, controller.catalogueList1$[index]));
-            })),
-          ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5.0),
+              child: Obx((() {
+                return GridView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: controller.catalogueList1$.length,
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 12.0,
+                      crossAxisSpacing: 10.0,
+                      childAspectRatio: 1,
+                    ),
+                    itemBuilder: (context, index) => _ListItemCatalogue1(
+                        context, controller.catalogueList1$[index]));
+              })),
+            ),
         ),
       ),
     ]);
@@ -181,9 +173,9 @@ class CatalogosPage extends GetView<CatalogosController> {
       child: Column(
         children: [
           GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(context, '/catalogs-list');
-            },
+      onTap: () {
+        Navigator.pushNamed(context, '/catalogs-list');
+      },
             child: ClipRRect(
                 borderRadius: BorderRadius.circular(10),
                 child: Stack(children: [
@@ -207,5 +199,10 @@ class CatalogosPage extends GetView<CatalogosController> {
         ],
       ),
     );
+
   }
+
+
+
+  
 }
