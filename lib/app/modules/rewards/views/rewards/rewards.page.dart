30a2970900bcfdbecc3c1a5d671be40fb72a2d98
avelay <avelay@jexbit.mx>
2022-09-22 08:59:30 -0500

    BUG EN REWARD

--- lib/app/modules/rewards/views/rewards/rewards.page.dart
+++ lib/app/modules/rewards/views/rewards/rewards.page.dart
@@ -175,11 +175,13 @@ class RewardsPage extends GetView<RewardsController> {
           ? controller.pointsLevel$.first.discountPercentage!.toDouble() / 100
           : 0,
       barRadius: const Radius.circular(15),
-      progressColor: controller.pointsLevel$.first.initialPoints! <= 100
-          ? const Color(0xFF43578d)
-          : controller.pointsLevel$[1].initialPoints! <= 500
-              ? Color(0xffffc600)
-              : Color.fromARGB(255, 0, 1, 4),
+      progressColor: controller.pointsLevel$.isNotEmpty
+          ? controller.pointsLevel$.first.initialPoints! <= 100
+              ? const Color(0xFF43578d)
+              : controller.pointsLevel$[1].initialPoints! <= 500
+                  ? Color(0xffffc600)
+                  : Color.fromARGB(255, 0, 1, 4)
+          : Colors.transparent,
     ).paddingSymmetric(vertical: 1, horizontal: 0);
   }
 
